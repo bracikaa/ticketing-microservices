a690b579e8661b831327b9cc5520b69e538fe664
Mehmed <mehmed_duhovic@hotmail.com>
2021-05-04 00:14:41 +0200

    Done with Auth Service, Created some Middlewares, wrote K8S Config files

---
+++ auth/src/middleware/current-user.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export interface UserPayload {
+  id: string;
+  email: string;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;    
+    }
+  }
+}
+
+export const currentUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (error) {
+    if (!req.session?.jwt) {
+      return next();
+    }
+
+    try {
+      const payload = jwt.verify(
+        req.session.jwt,
+        process.env.JWT_KEY!
+      ) as UserPayload;
+
+      req.currentUser = payload;
+    } catch (err) {}
+
+    return next();
+  }
+};
---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,11 @@
+import express, { json } from "express";
+import jwt from "jsonwebtoken";
+import { currentUser } from "../middleware/current-user";
+
+const router = express.Router();
+
+router.get("/api/users/currentuser", currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
+});
+
+export { router as currentUserRouter };
