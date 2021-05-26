8fee17d9cb25b1fe6fd265fb0a9fac2e39dd591b
Mehmed <mehmed_duhovic@hotmail.com>
2021-05-26 23:12:58 +0200

    use request hook

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,14 @@
-export default () => {
-  return <h1>Landing Page</h1>;
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log("I am in the component");
+  return <h1>{currentUser} Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get("/api/users/currentuser");
+
+  return response.data;
+};
+
+export default LandingPage;
