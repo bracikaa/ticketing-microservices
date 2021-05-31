6c2aa01cab4edf567d504a039363e88e55a4283c
Mehmed <mehmed_duhovic@hotmail.com>
2021-05-31 10:43:05 +0200

    index page changes

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,27 @@
 import axios from "axios";
 
 const LandingPage = ({ currentUser }) => {
-  console.log("I am in the component");
-  return <h1>{currentUser} Landing Page</h1>;
+  return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
-  const response = await axios.get("/api/users/currentuser");
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === "undefined") {
+    //when are on the server
+    const response = await axios.get(
+      "http://ingress-nginx-controller.kube-system.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers
+      }
+    );
 
-  return response.data;
+    return response.data;
+  } else {
+    // we are on the browser
+    const response = await axios.get("/api/users/currentuser");
+    return response.data;
+  }
+
+  return {};
 };
 
 export default LandingPage;
